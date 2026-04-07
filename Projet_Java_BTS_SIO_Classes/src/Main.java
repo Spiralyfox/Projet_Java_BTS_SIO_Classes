6cf8ad3f8e5881ff472641669d1a0c9ffa046c94
Spiralyfox <dauriacmatteo@gmail.com>
2026-04-07 14:25:10 +0200

    2.4 Mission 4 : Introduction aux objets

--- Projet_Java_BTS_SIO_Classes/src/Main.java
+++ Projet_Java_BTS_SIO_Classes/src/Main.java
@@ -10,6 +10,25 @@ public class Main {
         System.out.println("Voiture | Marque : " + marque + " Modèle : " + modele + " Année : " + annee + " Prix : " + prix);
          */
 
+        Voiture v1 = new Voiture();
+        Voiture v2 = new Voiture();
+        Voiture v3 = new Voiture();
+
+        v1.marque = "Peugeot";
+        v2.marque = "Renault";
+        v3.marque = "Toyota";
+
+        v1.modele = "208";
+        v2.modele = "Clio";
+        v3.modele = "Yaris";
+
+        v1.annee = 2021;
+        v2.annee = 2000;
+        v3.annee = 2015;
+
+        v1.prix = 14500;
+        v2.prix = 3500;
+        v3.prix = 9800;
 
     }
 }
